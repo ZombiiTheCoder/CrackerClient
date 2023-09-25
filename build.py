d48cda2d362982359de36e2f55e2d4a1cd5d723e
Logan Abernathy <MaximumUltimateDestroyer@gmail.com>
2023-09-25 13:36:57 -0700

    Added error handling, file operation optimization.

--- build.py
+++ build.py
@@ -1,27 +1,38 @@
 import os
+
+def remove_file(file_path):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+def copy_file(source, destination):
+    with open(source, "rb") as src_file, open(destination, "wb") as dest_file:
+        dest_file.write(src_file.read())
+
+# Compiling Dev - Go
 print("Compiling Dev - Go")
 os.chdir("src")
-try: os.remove("www/favicon.ico")
-except: True
-try: os.remove("winres/icon.png")
-except: True
-f=open("data/imgs/dev_logo.ico", "rb"); f2=open("www/favicon.ico", "wb"); f2.write(f.read()); f2.close(); f.close()
-f=open("data/imgs/dev_logo.png", "rb"); f2=open("winres/icon.png", "wb"); f2.write(f.read()); f2.close(); f.close()
+
+remove_file("www/favicon.ico")
+remove_file("winres/icon.png")
+
+copy_file("data/imgs/dev_logo.ico", "www/favicon.ico")
+copy_file("data/imgs/dev_logo.png", "winres/icon.png")
+
 os.system("go install github.com/tc-hib/go-winres@latest")
-# os.system("go-winres simply --icon \"data/imgs/dev_logo.png\" --manifest gui")
 os.system("go-winres make")
 os.system("go build -ldflags=\"-X main.edition=dev\" -o ../bin/Cracker-Client-Dev.exe")
 
+# Compiling Stable - Go
 print("Compiling Stable - Go")
-try: os.remove("www/favicon.ico")
-except: True
-try: os.remove("winres/icon.png")
-except: True
-f=open("data/imgs/logo.ico", "rb"); f2=open("www/favicon.ico", "wb"); f2.write(f.read()); f2.close(); f.close()
-f=open("data/imgs/logo.png", "rb"); f2=open("winres/icon.png", "wb"); f2.write(f.read()); f2.close(); f.close()
+
+remove_file("www/favicon.ico")
+remove_file("winres/icon.png")
+
+copy_file("data/imgs/logo.ico", "www/favicon.ico")
+copy_file("data/imgs/logo.png", "winres/icon.png")
+
 os.system("go install github.com/tc-hib/go-winres@latest")
 os.system("go-winres make")
-# os.system("go-winres simply --icon \"data/imgs/dev_logo.png\" --manifest gui")
 os.system("go build -ldflags=\"-H windowsgui\" -o ../bin/Cracker-Client.exe")
 
 os.chdir("../bin")
